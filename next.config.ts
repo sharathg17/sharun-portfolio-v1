6b7831c002f7e47a28bd5ea0a2cadb65fe703f15
Sharath Ganga <sganga@Sharaths-MacBook-Air.local>
2026-06-30 20:28:00 +0530

    Remove basePath to fix custom domain asset loading

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/sharun-portfolio-v1' : '',
   images: {
     unoptimized: true,
   },
